c6d72e0eb3e455d988c29567984a764a30f9cc2d
bksy-lcy <1161067272@qq.com>
2022-01-18 01:51:29 +0800

    real_video

--- net/real_video/to_tile.sh
+++ net/real_video/to_tile.sh
@@ -1,38 +1,30 @@
 # NYC8K_VR360.mp4 7680x3840
-#ffmpeg -i ~/srt_tmp/videos/NYC8K_VR360.mp4 -ss 00:00:00 -to 00:01:00 -vf crop=3840:1920:0:0 tile0.mp4
-mkdir ~/srt_tmp/videos/NYC8K
 let i_w=7680
 let i_h=3840
-# 1x1
-let x=1
-let y=1
-let o_w=i_w/y
-let o_h=i_h/x
-echo ${o_w},${o_h}
-mkdir ~/srt_tmp/videos/NYC8K/tile_${x}_${y}
-for((i=0;i<${x};i++))
-do
-    for((j=0;j<${y};j++))
-    do
-        let s_x=j*o_w
-        let s_y=i*o_h
-        let id=i*y+j
-        # echo ${o_w},${o_h},${s_x},${s_y},${id}
-        # ffmpeg -i ~/srt_tmp/videos/NYC8K_VR360.mp4 -vf crop=${o_w}:${o_h}:${s_x}:${s_y} ~/srt_tmp/videos/NYC8K/tile_${x}_${y}/tile${id}.mp4
-    done
-done
-# 2x3
-# 4x6
-# 8x12
-# 16x24
-#for k in 1 2 4 8
-for k in 1
+
+# # mkdir
+# mkdir ~/srt_tmp/videos/NYC8K
+# mkdir ~/srt_tmp/videos/NYC8K/tile_1_1
+# for k in 1 2 4 8
+# do 
+#     let x=k*2
+#     let y=k*3
+#     mkdir ~/srt_tmp/videos/NYC8K/tile_${x}_${y}
+# done
+
+# #ffmpeg -i ~/srt_tmp/videos/NYC8K_VR360.mp4 -ss 00:00:00 -to 00:01:00 -vf crop=3840:1920:0:0 tile0.mp4
+
+# # 1x1
+# ffmpeg -i ~/srt_tmp/videos/NYC8K_VR360.mp4 -y ~/srt_tmp/videos/NYC8K/tile_1_1/tile0.mp4
+# 2x3, 4x6, 8x12, 16x24
+# for k in 1 2 4 8
+for k in 2 4 8
 do 
     let x=k*2
     let y=k*3
     let o_w=i_w/y
     let o_h=i_h/x
-    echo ${o_w},${o_h}
+    # echo ${o_w},${o_h}
     mkdir ~/srt_tmp/videos/NYC8K/tile_${x}_${y}
     for((i=0;i<${x};i++))
     do
@@ -42,7 +34,7 @@ do
             let s_y=i*o_h
             let id=i*y+j
             # echo ${o_w},${o_h},${s_x},${s_y},${id}
-            ffmpeg -i ~/srt_tmp/videos/NYC8K_VR360.mp4 -ss 00:00:00 -to 00:01:00 -vf crop=${o_w}:${o_h}:${s_x}:${s_y} ~/srt_tmp/videos/NYC8K/tile_${x}_${y}/tile${id}.mp4
+            ffmpeg -i ~/srt_tmp/videos/NYC8K_VR360.mp4 -vf crop=${o_w}:${o_h}:${s_x}:${s_y} -y ~/srt_tmp/videos/NYC8K/tile_${x}_${y}/tile${id}.mp4
         done
     done
 done
